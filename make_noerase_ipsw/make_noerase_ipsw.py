695379858a8fe01fc67b3443ded9608101b38c26
allenfoxtc <allenfoxtc@gmail.com>
2016-09-18 00:15:00 +0800

    Update make_noerase_ipsw.py
    Add support for iOS 10 ipsw (multi-device supported ipsw).

--- make_noerase_ipsw/make_noerase_ipsw.py
+++ make_noerase_ipsw/make_noerase_ipsw.py
@@ -1,5 +1,6 @@
 import os, zipfile, sys, tempfile, shutil, plistlib, traceback
 
+
 def recursive_zip(zipf, basePath, zipPath=""):
     for fname in os.listdir(os.path.join(basePath, zipPath)):
         izPath = os.path.join(zipPath, fname)
@@ -9,44 +10,51 @@ def recursive_zip(zipf, basePath, zipPath=""):
         elif os.path.isdir(iPath):
             recursive_zip(zipf, basePath, izPath)
 
+
 def fix_ramdisk_dict(ramdisks):
-        ramdisks["User"] = ramdisks["Update"]
-        del ramdisks["Update"]
+    ramdisks["User"] = ramdisks["Update"]
+    del ramdisks["Update"]
+
 
 def fix_build_manifest(ipswDir):
     buildManifestPlist = os.path.join(ipswDir, "BuildManifest.plist")
     if not os.path.exists(buildManifestPlist):
         print "No BuildManifest file"
         return
-    
+
     buildManifest = plistlib.readPlist(buildManifestPlist)
-    
     buildIdentities = buildManifest["BuildIdentities"]
 
-    assert len(buildIdentities) == 2
+    assert len(buildIdentities) % 2 == 0
 
-    restoreIndex = -1
-    updateIndex = -1
+    # fix for ios 10
+    restoreIndex = []
+    updateIndex = []
 
     for i in range(len(buildIdentities)):
         biInfo = buildIdentities[i]["Info"]
-        print "Build identity %i: %s" % (i, biInfo["RestoreBehavior"])
+        # print "Build identity %i: %s" % (i, biInfo["RestoreBehavior"])
         if biInfo["RestoreBehavior"] == "Erase":
-            restoreIndex = i
+            restoreIndex.append(i)
         elif biInfo["RestoreBehavior"] == "Update":
-            updateIndex = i
+            updateIndex.append(i)
 
-    assert (restoreIndex + updateIndex == 1)
-    
-    buildIdentities.pop(restoreIndex)
-    updateBiInfo = buildIdentities[0]["Info"];
-    
-    assert  "Update" == updateBiInfo["RestoreBehavior"]
-    updateBiInfo["RestoreBehavior"] = "Erase";
-    
-    assert "UpdateRamDisk" == updateBiInfo["VariantContents"]["RestoreRamDisk"]
-    updateBiInfo["VariantContents"]["RestoreRamDisk"] = "CustomerRamDisk"
-    
+    assert (len(restoreIndex) + len(updateIndex) == len(buildIdentities))
+
+    restoreIndex.reverse()
+
+    for restoreindex in restoreIndex:
+        buildIdentities.pop(restoreindex)
+
+    for k in range(len(buildIdentities)):
+        updateBiInfo = buildIdentities[k]["Info"]
+
+        assert "Update" == updateBiInfo["RestoreBehavior"]
+        updateBiInfo["RestoreBehavior"] = "Erase"
+
+        assert "UpdateRamDisk" == updateBiInfo["VariantContents"]["RestoreRamDisk"]
+        updateBiInfo["VariantContents"]["RestoreRamDisk"] = "CustomerRamDisk"
+
     plistlib.writePlist(buildManifest, buildManifestPlist)
 
     print "Written BuildManifest.plist"
@@ -55,9 +63,9 @@ def fix_build_manifest(ipswDir):
 def make_upgrade_only(ipswDir):
     restorePlist = os.path.join(ipswDir, "Restore.plist")
     restore = plistlib.readPlist(restorePlist)
-    
+
     restoreRamDisks = restore["RestoreRamDisks"]
-    
+
     print "Update RD: %s, restore RD: %s" % (restoreRamDisks["Update"], restoreRamDisks["User"])
 
     updateRamdisk = restoreRamDisks["Update"]
@@ -67,16 +75,17 @@ def make_upgrade_only(ipswDir):
     print "Deleted RESTORE ramdisk file: %s" % restoreRamdisk
 
     fix_ramdisk_dict(restoreRamDisks)
-    
+
     for platformName, platInfo in restore["RamDisksByPlatform"].items():
         print "Platform: %s, update RD: %s, restore RD: %s" % (platformName, platInfo["Update"], platInfo["User"])
         fix_ramdisk_dict(platInfo)
-        
+
     plistlib.writePlist(restore, restorePlist)
     print "Written Restore.plist"
 
     fix_build_manifest(ipswDir)
-    
+
+
 def has_build_manifest(ipsw):
     try:
         ipsw.getinfo("BuildManifest.plist")
@@ -84,11 +93,22 @@ def has_build_manifest(ipsw):
     except KeyError:
         return False
 
+
+def ipsw_info(ipswDir):
+    buildManifestPlist = os.path.join(ipswDir, "BuildManifest.plist")
+    if not os.path.exists(buildManifestPlist):
+        return
+
+    buildManifest = plistlib.readPlist(buildManifestPlist)
+    print "ProductVersion: %s, ProductBuildVersion: %s, SupportedProductTypes: %s" % (
+        buildManifest["ProductVersion"], buildManifest["ProductBuildVersion"], buildManifest["SupportedProductTypes"])
+
+
 def main():
     if len(sys.argv) <= 1:
         print "Usage: %s <ipsw file>" % sys.argv[0]
         return
-    
+
     origIpsw = sys.argv[1]
 
     print "Original IPSW file: %s" % origIpsw
@@ -100,37 +120,39 @@ def main():
     except:
         print "Failed to open", origIpsw
         return
-    
+
     if not has_build_manifest(ipsw):
         print "IPSW for an unsupported device: BuildManifest.plist missing!"
         return
-    
+
     tempDir = tempfile.mkdtemp()
     print "Unpacking to %s ..." % tempDir
     ipsw.extractall(tempDir)
     ipsw.close()
+
+    ipsw_info(tempDir)
 
     make_upgrade_only(tempDir)
 
     newPath = os.path.split(origIpsw)
     newIpsw = os.path.join(newPath[0], "UPG_" + newPath[1])
     print "Packing upgrade-only IPSW to %s ..." % newIpsw
-    ipswUp = zipfile.ZipFile(newIpsw, mode='w')
+    ipswUp = zipfile.ZipFile(newIpsw, mode='w', allowZip64=True)
     recursive_zip(ipswUp, tempDir)
     ipswUp.close()
 
     print "Cleaning up"
     shutil.rmtree(tempDir)
 
+
 if __name__ == '__main__':
     try:
         main()
         print
         print "Press Enter to exit."
-        blah = raw_input()        
+        blah = raw_input()
     except:
         traceback.print_exc()
-        print >>sys.stderr
-        print >>sys.stderr, "Press Enter to exit."
+        print >> sys.stderr
+        print >> sys.stderr, "Press Enter to exit."
         blah = raw_input()
-
